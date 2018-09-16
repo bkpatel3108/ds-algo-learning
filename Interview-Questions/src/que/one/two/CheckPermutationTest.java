e82d136b2090a362c63c504883ffb42be37400c1
bkpatel3108 <bkpatel3108@gmail.com>
2018-09-16 14:44:28 -0400

    remove unnecessary project

--- Interview-Questions/src/que/one/two/CheckPermutationTest.java
+++
@@ -1,9 +0,0 @@
-package que.one.two;
-
-public class CheckPermutationTest {
-
-	public static void main(String[] args)
-	{
-		System.out.println(CheckPermutation.checkPermutation("abcdfga", "adae")); 
-	}
-}
